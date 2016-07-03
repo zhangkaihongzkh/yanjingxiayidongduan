dcd5a85caa0f6f6ce4cd0e40136cecca0f509d47
zhangkaihongzkh <953628520@qq.com>
2016-07-03 14:23:38 +0800

    滑动组件完成 同时修改部分样式

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,8 @@
 //构造函数
 function Slider(dom){
-	//构造函数所需的参数
+	//构造函数需要的参数
 	this.wrap = dom;
-	this.list = wrap.getElementsByTagName("l1");
+	this.list = dom.getElementsByTagName('li');
 	//构造三步骤
 	this.init();
 	this.renderDOM();
@@ -11,86 +11,153 @@ function Slider(dom){
 
 //第一步 -- 初始化
 Slider.prototype.init = function(){
-	//设定每一页的宽高
-	this.scaleWidth = window.innerWidth;
-	this.scaleHeight = window.innerHeight;
-	//设定初始的索引值为0
+	//设定窗口比率
+	this.radio = window.innerHeight/window.innerWidth;
+	//设定一页的宽度
+	this.scaleW = window.innerWidth;
+	this.scaleH = window.innerHeight;
+	//设定初始的索引值
 	this.idx = 0;
 };
 
-//第二步 -- 渲染DOM
+//第二部 -- 渲染DOM
 Slider.prototype.renderDOM = function(){
 	var wrap = this.wrap;
-	var list = wrap.getElementsByTagName("li");
-	var len = list.length;
+	var data = this.list;
+	var len  = data.length;
 
-	for(var i = 0;i<len;i++){
-		var item = list[i];
+	for (var i = 0; i < len; i++) {
+		var item = data[i];
+		var img  = item.getElementsByTagName('li')[0];
 		item.style.height = window.innerHeight + 'px';
-		item.style.width = window.innerWidth + 'px';
-		item.style.webkitTransform = 'translate3d(0,'+i*this.scaleHeight+'px,0)';
-		/*console.log('第'+i+'张的图片高度是'+i*this.scaleHeight+'px');
-		console.log('第'+i+'张的图片宽度是'+this.scaleWidth+'px');*/
+		item.style.webkitTransform = 'translate3d(0,'+i*this.scaleH+'px, 0)';
 	}
-
+	var lis = wrap.getElementsByTagName('li');
+	for( i = 0; i < lis.length; i++){
+		lis[i].style.width = window.innerWidth +'px';
+	}
+	wrap.style.width = window.innerWidth +'px';
 	wrap.style.height = window.innerHeight + 'px';
-	wrap.style.width = window.innerWidth + 'px';
 };
-Slider.prototype.goIndex(n){
+Slider.prototype.goIndex = function(n){
 	var idx = this.idx;
 	var wrap = this.wrap;
-	var list = wrap.getElementsByTagName("li");
-	var len = list.length;
+	var lis = wrap.getElementsByTagName('li');
+	var len = lis.length;
 	var cidx;
 
-	//如果传的的2,3数字,可以直接滑动到该索引
+	//如果传数字 2,3 之类可以使得直接滑动到该索引
 	if(typeof n == 'number'){
 		cidx = idx;
-	}else if(typeof n == 'string'){		//传的是字符串
-		cidx = n*1;
+	//如果是传字符则为索引的变化
+	}else if(typeof n == 'string'){
+		cidx = idx + n*1;
 	}
 
-	//当索引超出值时
+	//当索引右超出
 	if(cidx > len-1){
-		cidx = len -1
+		cidx = len - 1;
+	//当索引左超出	
 	}else if(cidx < 0){
 		cidx = 0;
 	}
-	//保留当前值
+
+	//保留当前索引值
 	this.idx = cidx;
 
-	//改变动画的过渡方式，从无动画到有动画
-	list[cidx].style.webkitTransform = '-webkit-transform 0.2s ease-out';
-	list[cidx-1] && (list[cidx-1].style.webkitTransform = '-webkit-transform 0.2s ease-out');
-	list[cidx+1] && (list[cidx+1].style.webkitTransform = '-webkit-transform 0.2s ease-out');
+	//改变过渡的方式，从无动画变为有动画
+	lis[cidx].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+	lis[cidx-1] && (lis[cidx-1].style.webkitTransition = '-webkit-transform 0.2s ease-out');
+	lis[cidx+1] && (lis[cidx+1].style.webkitTransition = '-webkit-transform 0.2s ease-out');
 
-	//动画后对应的位移
-	list[cidx].style.webkitTransform = 'translate3d(0,0,0)';
-	list[cidx-1] && (list[cidx-1].style.webkitTransform = 'translate3d(0,'+this.scaleHeight+'px,0)');
-	list[cidx+1] && (list[cidx+1].style.webkitTransform = 'translate3d(0,'+this.scaleHeight+'px,0)');
+	//改变动画后所应该的位移值
+	lis[cidx].style.webkitTransform = 'translate3d(0, 0, 0)';
+	lis[cidx-1] && (lis[cidx-1].style.webkitTransform = 'translate3d(0, -'+this.scaleH+'px, 0)');
+	lis[cidx+1] && (lis[cidx+1].style.webkitTransform = 'translate3d(0, ' +this.scaleH+'px, 0)');
 };
-
-//第三步 -- 绑定DOM事件
-Slider.prototype.bindDOM = function(){
+//第三步 -- 绑定 DOM事件
+Slider.prototype.bindDOM = function() {
+	var self = this;
+	var scaleW = self.scaleW;
+	var scaleH = self.scaleH;
+	var len = self.list.length;
 	var wrap = this.wrap;
-	var list = wrap.getElementsByTagName("li");
-
-	var scaleH = self.scaleHeight;
-	var scale = self.scaleWidth;
-	var len = list.length;
 
 	var startHandler = function(evt){
+
+		//记录刚刚开始按下的时间
+		self.startTime = new Date() * 1;
+
+		//记录手指按下的坐标
+		self.startY = evt.touches[0].pageY;
+
+		//清除偏移量
+		self.offsetY = 0;
+
 		//事件对象
+		var target = evt.target;
+		while(target.nodeName != 'LI' && target.nodeName != 'BODY'){
+			target = target.parentNode;
+		}
+		self.target = target;
+
 	};
-	//手指移动事件
+
+	//手指移动的处理事件
 	var moveHandler = function(evt){
+		//兼容chrome android，阻止浏览器默认行为
+		evt.preventDefault();
 
+		//计算手指的偏移量
+		self.offsetY = evt.targetTouches[0].pageY - self.startY;
+		var lis = wrap.getElementsByTagName('li');
+		//起始索引
+		var i = self.idx - 1;
+		//结束索引
+		var m = i + 3;
+
+		//最小化改变DOM属性
+		for(i; i < m; i++){
+			lis[i] && (lis[i].style.webkitTransition = '-webkit-transform 0s ease-out');
+			lis[i] && (lis[i].style.webkitTransform = 'translate3d(0,'+ ((i-self.idx)*self.scaleH + self.offsetY) +'px, 0)');
+		}
 	};
-	//手抬起来的时候
+
+	//手指抬起的处理事件
 	var endHandler = function(evt){
+		evt.preventDefault();
 
+		//边界就翻页值
+		var boundary = scaleW/6;
+
+		//手指抬起的时间值
+		var endTime = new Date() * 1;
+
+		//所有列表项
+		var lis = wrap.getElementsByTagName('li');
+
+		//当手指移动时间超过300ms 的时候，按位移算
+		if(endTime - self.startTime > 300){
+			if(self.offsetY >= boundary){
+				self.goIndex('-1');
+			}else if(self.offsetY < 0 && self.offsetY < -boundary){
+				self.goIndex('+1');
+			}else{
+				self.goIndex('0');
+			}
+		}else{
+			//优化
+			//快速移动也能使得翻页
+			if(self.offsetY > 50){
+				self.goIndex('-1');
+			}else if(self.offsetY < -50){
+				self.goIndex('+1');
+			}else{
+				self.goIndex('0');
+			}
+		}
 	};
-	//绑定事件
+
 	this.wrap.addEventListener('touchstart', startHandler);
 	this.wrap.addEventListener('touchmove', moveHandler);
 	this.wrap.addEventListener('touchend', endHandler);
